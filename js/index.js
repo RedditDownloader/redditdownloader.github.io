f834a70e6a5149a488207c5a3e2b41a4acb57f31
UniquePassive <prozet@live.se>
2017-08-07 17:05:57 +0200

    Remove redundant length check

--- js/index.js
+++ js/index.js
@@ -130,67 +130,65 @@ function download(maxImageCount, anchor) {
 
                 var downloadedCountNow = 0;
 
-                if (children.length > 0) {
-                    for (var i = 0; i < children.length; i++) {
-                        var post = children[i].data;
+                for (var i = 0; i < children.length; i++) {
+                    var post = children[i].data;
 
-                        /* Only download if there's a thumbnail */
-                        if (post.thumbnail_width === null) {
+                    /* Only download if there's a thumbnail */
+                    if (post.thumbnail_width === null) {
+                        continue;
+                    }
+
+                    /* Respect user's nsfw option */
+                    if (!includeNsfw && post.over_18) {
+                        continue;
+                    }
+
+                    /* Check if there are any images, there should be, but let's make sure */
+                    if (post.preview === undefined || post.preview.images.length == 0) {
+                        continue;
+                    }
+
+                    /* Don't download images that come from posts with greater/less score than inputted */
+                    if (restrictByScore) {
+                        if ((restrictByScoreType === "ge" && post.score < restrictByScoreValue)
+                                || (restrictByScoreType === "le" && post.score > restrictByScoreValue)) {
                             continue;
+                        } 
+                    }
+
+                    var url = post.preview.images[0].source.url;
+
+                    if (isUrlFileFormatAccepted(url)) {
+                        /* Force https */
+                        if (url.startsWith("http:")) {
+                            url = url.replace("http:", "https:");
                         }
 
-                        /* Respect user's nsfw option */
-                        if (!includeNsfw && post.over_18) {
-                            continue;
-                        }
+                        toDownloadCount++;
+                        downloadedCountNow++;
+                        updateUI();
 
-                        /* Check if there are any images, there should be, but let's make sure */
-                        if (post.preview === undefined || post.preview.images.length == 0) {
-                            continue;
-                        }
+                        downloadImageAsBase64(url, post, function(url, post, data) {
+                            var destinationFileName;
 
-                        /* Don't download images that come from posts with greater/less score than inputted */
-                        if (restrictByScore) {
-                            if ((restrictByScoreType === "ge" && post.score < restrictByScoreValue)
-                                    || (restrictByScoreType === "le" && post.score > restrictByScoreValue)) {
-                                continue;
-                            } 
-                        }
-
-                        var url = post.preview.images[0].source.url;
-
-                        if (isUrlFileFormatAccepted(url)) {
-                            /* Force https */
-                            if (url.startsWith("http:")) {
-                                url = url.replace("http:", "https:");
+                            if (nameFormat === "file-name") {
+                                destinationFileName = getFileNameWithExtension(url);
+                            } else if (nameFormat === "post-id") {
+                                destinationFileName = post.name + getFileExtension(url);
+                            } else {
+                                /* default: post-name */
+                                var regex = /[^\/]+(?=\/$|$)/g;
+                                var postName = regex.exec(post.permalink)[0];
+                                destinationFileName = postName + getFileExtension(url);
                             }
 
-                            toDownloadCount++;
-                            downloadedCountNow++;
+                            zip.file(destinationFileName, data, { base64: true });
+                            downloadedCount++;
                             updateUI();
+                        });
 
-                            downloadImageAsBase64(url, post, function(url, post, data) {
-                                var destinationFileName;
-
-                                if (nameFormat === "file-name") {
-                                    destinationFileName = getFileNameWithExtension(url);
-                                } else if (nameFormat === "post-id") {
-                                    destinationFileName = post.name + getFileExtension(url);
-                                } else {
-                                    /* default: post-name */
-                                    var regex = /[^\/]+(?=\/$|$)/g;
-                                    var postName = regex.exec(post.permalink)[0];
-                                    destinationFileName = postName + getFileExtension(url);
-                                }
-
-                                zip.file(destinationFileName, data, { base64: true });
-                                downloadedCount++;
-                                updateUI();
-                            });
-
-                            if (downloadedCountNow == maxImageCount) {
-                                break;
-                            }
+                        if (downloadedCountNow == maxImageCount) {
+                            break;
                         }
                     }
                 }
