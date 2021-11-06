17a5d7c116ff275ce74de21f54a4d1e6dc385085
Alexander Henne <prozet@live.se>
2021-11-06 21:26:00 +0100

    Add support for more video formats

--- js/index.js
+++ js/index.js
@@ -595,7 +595,9 @@ function isDirectImageUrl(url) {
 
 function isDirectVideoUrl(url) {
     url = url.toLowerCase();
-    return url.indexOf(".mp4") !== -1;
+    return url.indexOf(".mp4") !== -1 || url.indexOf(".mkv") !== -1
+        || url.indexOf(".mov") !== -1 || url.indexOf(".avi") !== -1
+        || url.indexOf(".webm") !== -1;
 }
 
 function isDirectGifUrl(url) {
